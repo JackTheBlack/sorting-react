8bcc9ff5358db655f275390e74a2e65dab3e2930
JackTheBlack <ascona.nicolas@gmail.com>
2021-07-03 13:38:23 -0300

    antd table
    doesnt re-render after update the order yet

--- sort/src/components/sort/index.js
+++ sort/src/components/sort/index.js
@@ -1,8 +1,13 @@
 import React, { Component, useState, useEffect } from "react";
+import { Button, Table, Tag, Space } from "antd";
+import "antd/dist/antd.css";
 
 function SORT({ articles }) {
   const [aux, setAux] = useState([...articles]);
   const [filter, setFilter] = useState("upvoted");
+
+  const { Column } = Table;
+
   const upVoted = () => {
     setFilter(" ");
     setFilter("upvoted");
@@ -17,6 +22,7 @@ function SORT({ articles }) {
       console.log(filter);
       setAux(" ");
       setAux([...articles]);
+
       const newArr = aux.sort((a, b) => b.upvotes - a.upvotes);
       setAux(newArr);
     } else if (filter === "date") {
@@ -30,12 +36,18 @@ function SORT({ articles }) {
 
   return (
     <div>
-      <button data-testid="most-recent-link" onClick={recent}>
+      <Button type="primary" data-testid="most-recent-link" onClick={recent}>
         Most Recent
-      </button>
-      <button data-testid="most-upvoted-link" onClick={upVoted}>
+      </Button>
+      <Button type="primary" data-testid="most-upvoted-link" onClick={upVoted}>
+        {" "}
         Most UpVoted
-      </button>
+      </Button>
+      <Table dataSource={aux}>
+        <Column title="Title" dataIndex="title" key="title" />
+        <Column title="Upvotes" dataIndex="upvotes" key="upvotes" />
+        <Column title="Date" dataIndex="date" key="date" />
+      </Table>
       <table>
         <tr>
           <th>Title</th>
